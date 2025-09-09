88e9085f5c777a1e00e4d8711fd737d4233ffe72
狐衰 <hushuai@easonmacbook.local>
2025-09-09 12:32:08 -0400

    refactored to extract helper method and fixed while loop

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,30 +7,25 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        doFizzBuzz();
+    }
 
-            // Find out which numbers divide i.
+    private static void doFizzBuzz() {
+        int i = 1;
+        while (i < 100) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
-
                 System.out.println("Fizz Buzz");
-
             } else if (divisibleBy3) {
-
                 System.out.println("Fizz");
-
             } else if (divisibleBy5) {
-
                 System.out.println("Buzz");
-
             } else {
-
                 System.out.println(i);
-
             }
+            i++;
         }
     }
 }
