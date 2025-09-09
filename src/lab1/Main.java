0f6eacd2a9ae99b8fd5179ad938140195f3066be
xfoxsad <x.hu.shuai@gmail.com>
2025-09-09 12:54:05 -0400

    sec times with updated Main and new multiples but no codes

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,5 +3,6 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz();
+        FizzBuzz.main(new String[0]);
     }
 }
---
+++ src/lab1/Multiples.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+
+    }
+}
